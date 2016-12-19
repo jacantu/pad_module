84389c789468bc989bb8247cbb6cf731212274bf
Adrian Cantu <ja.cantu@my.codergv.org>
2016-12-19 11:10:47 -0600

    fixed readme and test.js

---
+++ README.md
@@ -0,0 +1,7 @@
+#pad_module
+
+* This module makes left pad a string
+
+#Install
+
+* npm install pad_module
--- test.js
+++ test.js
@@ -2,4 +2,4 @@ var lpad = require('./module.js')
 //var dir = process.argv[2]
 //var filterStr = process.argv[3]
 
-lpad('hello', 5, callback);
+console.log(lpad('hello', 20));
